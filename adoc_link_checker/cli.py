612786c884dc56f11ddf459697b8402f595b6927
dhrions <aturing@laposte.net>
2025-08-28 13:09:09 +0200

    💡 Translate

--- adoc_link_checker/cli.py
+++ adoc_link_checker/cli.py
@@ -15,52 +15,52 @@ from adoc_link_checker.config import TIMEOUT, MAX_WORKERS, DELAY, BLACKLIST, OUT
     "--timeout",
     type=int,
     default=TIMEOUT,
-    help=f"Timeout pour les requêtes HTTP (secondes). [Default: {TIMEOUT}]",
+    help=f"Timeout for HTTPs requests (seconds). [Default: {TIMEOUT}]",
 )
 @click.option(
     "--max-workers",
     type=int,
     default=MAX_WORKERS,
-    help=f"Nombre maximal de threads pour le traitement parallèle. [Default: {MAX_WORKERS}]",
+    help=f"Maximal number of threads for parallel processing [Default: {MAX_WORKERS}]",
 )
 @click.option(
     "--delay",
     type=float,
     default=DELAY,
-    help=f"Délai entre chaque requête (secondes). [Default: {DELAY}]",
+    help=f"Delay between each request (seconds). [Default: {DELAY}]",
 )
 @click.option(
     "--output",
     type=click.Path(),
     default=OUTPUT_FILE,
-    help=f"Fichier de sortie JSON pour les liens brisés. [Default: {OUTPUT_FILE}]",
+    help=f"JSON ouput file for broken links. [Default: {OUTPUT_FILE}]",
 )
 @click.option(
     "--blacklist",
     type=str,
     multiple=True,
     default=[],
-    help="Domaine à ignorer (peut être spécifié plusieurs fois).",
+    help="Domain to ignore (can be specified multiple times).",
 )
 @click.option(
     "--verbose",
     is_flag=True,
-    help="Active le mode verbeux (DEBUG).",
+    help="Active verbose mode (DEBUG).",
 )
 @click.option(
     "--exclude-from",
     type=click.Path(exists=True, dir_okay=False),
     default=None,
-    help="Fichier contenant la liste des liens à exclure (un lien par ligne).",
+    help="File with list of links to exclude (a link by line).",
 )
 @click.version_option(version="1.0.0")
 def cli(root_dir, timeout, max_workers, delay, output, blacklist, verbose, exclude_from):
-    """Vérifie les liens brisés dans les fichiers .adoc du répertoire ROOT_DIR."""
+    """Check broken links in .adoc files"""
     if verbose:
         LOGGING_CONFIG["level"] = logging.DEBUG
     logging.basicConfig(level=LOGGING_CONFIG["level"], format=LOGGING_CONFIG["format"], force=True)
     logger = logging.getLogger(__name__)
-    logger.info(f"🔍 Démarrage de la vérification dans {os.path.abspath(root_dir)}")
+    logger.info(f"🔍 Begin check in {os.path.abspath(root_dir)}")
     run_check(
         root_dir=root_dir,
         max_workers=max_workers,
@@ -70,7 +70,7 @@ def cli(root_dir, timeout, max_workers, delay, output, blacklist, verbose, exclu
         blacklist=BLACKLIST + list(blacklist),
         exclude_from=exclude_from
     )
-    logger.debug(f"Fichier de sortie : {os.path.abspath(output)}")  # Debug
+    logger.debug(f"Output file : {os.path.abspath(output)}")  # Debug
 
 if __name__ == "__main__":
     cli()
