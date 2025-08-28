5abff1813f79dcea4e4a2b7a8a88cab2a9b456b4
dhrions <aturing@laposte.net>
2025-08-28 11:09:15 +0200

    🔧 Add setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.adoc", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="adoc-link-checker",  # Nom cohérent avec le README
-    version="0.1.0",           # Version harmonisée
+    name="adoc-link-checker",
+    version="0.1.0",
     author="dhrions",
     description="Un outil en ligne de commande pour vérifier les liens brisés dans les fichiers AsciiDoc.",
     long_description=long_description,
     long_description_content_type="text/asciidoc",
-    url="https://github.com/dhrons/utils",
+    url="https://github.com/dhrions/adoc-link-checker",
     packages=find_packages(),
     install_requires=[
         "certifi==2025.8.3",
@@ -22,25 +22,25 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "adoc-link-checker=adoc-link-checker.cli:cli",  # Nom cohérent
+            "adoc-link-checker=adoc_link_checker.cli:cli",
         ],
     },
     python_requires=">=3.8",
     license="MIT",  # À adapter selon ta licence
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Quality Assurance",
-        "Topic :: Text Processing :: Markup :: AsciiDoc",
+        # "Programming Language :: Python :: 3",
+        # "Programming Language :: Python :: 3.8",
+        # "Programming Language :: Python :: 3.9",
+        # "Programming Language :: Python :: 3.10",
+        # "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+        # "Intended Audience :: Developers",
+        # "Topic :: Software Development :: Quality Assurance",
+        # "Topic :: Text Processing :: Markup :: AsciiDoc",
     ],
     keywords="asciidoc link checker broken links",
     project_urls={
-        "Bug Tracker": "https://github.com/dhrons/utils/issues",
-        "Source Code": "https://github.com/dhrons/utils",
+        "Bug Tracker": "https://github.com/dhrons/adoc-link-checker/issues",
+        "Source Code": "https://github.com/dhrons/adoc-link-checker",
     },
 )
